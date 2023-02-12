8b6c85e49ada93dfa4a8491edaba48a5fa335670
Lucas <lucas.fn4@gmail.com>
2023-02-12 13:19:35 -0300

    Add math problem

---
+++ codeforces/69a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+int n;
+int sumOfForces[3] = {0, 0, 0};
+
+void getForceVector(){
+  int coordinate = 0;
+  for(int i = 0; i < 3; i++){
+    std::cin >> coordinate;
+    sumOfForces[i] += coordinate;
+  }
+}
+
+std::string bodyIsIdle(){
+  for(int i = 0; i < 3; i++){
+    if(sumOfForces[i] != 0){
+      return "NO\n";
+    }
+  }
+  return "YES\n";
+}
+
+int main(){
+  std::cin >> n;
+  int sum = 0;
+
+  while(n--){
+    getForceVector();
+  }
+
+  std::cout << bodyIsIdle();
+
+  return 0;
+}
