2b1a2163dcd9d4fa8a11352dc5455a90f5cdf650
Lucas <lucas.fn4@gmail.com>
2023-08-22 19:23:57 -0300

    Add binary search problem

---
+++ leetcode/search_insert_position.c
@@ -0,0 +1,24 @@
+int searchInsert(int* nums, int numsSize, int target){
+    // we'll solve it with a binary search
+    int left = 0;
+    int right = numsSize - 1;
+
+    // check if it's 
+    if(target == nums[left]) return left;
+    if(target == nums[right]) return right;
+    int mid = 0;
+
+    while(right > left + 1) {
+        mid = (left + right)/2;
+        if(nums[mid] == target) return mid; 
+        if(nums[mid] > target) 
+            right = mid;
+        else left = mid;
+    }
+    if(target > nums[right]) return right + 1;
+    if(target < nums[left]) {
+       if(left == 0) return left; 
+       else return left - 1;
+    }
+    return right;
+}
