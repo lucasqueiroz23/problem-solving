cd2b3b866524e7f7679b54f72205efedb64ee4a1
Lucas <lucas.fn4@gmail.com>
2024-02-24 20:31:31 -0300

    Adds leetcode problem

---
+++ leetcode/pascals-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ret;
+        ret.push_back({1});
+
+        for(int i = 1; i < numRows; i++) {
+            vector<int> new_arr = {1};
+            for(int j = 1; j < i ; j++) {
+                new_arr.push_back(ret[i-1][j] + ret[i-1][j-1]);
+            }
+            new_arr.push_back(1);
+            ret.push_back(new_arr);
+        }
+
+
+        return ret;
+    }
+};
