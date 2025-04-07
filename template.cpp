afce2847876bbc8f641290d3d9e864f233ba8bb6
Lucas Queiroz <lucas.fn4@gmail.com>
2025-04-07 18:58:08 -0300

    adds template for contests

---
+++ template.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs
+using ll = long long;
+using ii = pair<int,int>;
+using vi = vector<int>;
+using vii = vector<ii>;
+
+// common consts
+const vector<ii> dirs{
+    {-1, 0}, // west
+    {1, 0}, // east
+    {0, 1}, // north
+    {0, -1}, // south
+};
+
+// common macros
+
+// remember to declare the vector with a size.
+#define readvector(v) \
+    for(auto& val: v) \
+        cin >> val
+
+#define printvector(v) \
+    for(auto& val : v) \
+        cout << val << " "
+
+void solve() {
+
+}
+
+int main() {
+    // make input faster
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    solve();
+    return 0;
+}
