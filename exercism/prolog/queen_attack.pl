0f9bdbe8ec37b7bb48a02b479f383c0cfd8d327e
Lucas Queiroz <lucas.fn4@gmail.com>
2025-04-09 11:26:33 -0300

    adds basic logical programming exercises

---
+++ exercism/prolog/queen_attack.pl
@@ -0,0 +1,28 @@
+%! create(+DimTuple)
+%
+% The create/1 predicate succeeds if the DimTuple contains valid chessboard 
+% dimensions, e.g. (0,0) or (2,4).
+create((DimX, DimY)) :-
+    DimX >= 0,
+    DimX < 8,
+    DimY >= 0,
+    DimY < 8.
+
+
+%! attack(+FromTuple, +ToTuple)
+%
+% The attack/2 predicate succeeds if a queen positioned on ToTuple is 
+% vulnerable to an attack by another queen positioned on FromTuple.
+
+% same column 
+attack((FromX, FromY), (ToX, ToY)):-
+    FromX = ToX .
+
+% same row
+attack((FromX, FromY), (ToX, ToY)):-
+    FromY = ToY .
+
+% diagonal
+attack((FromX, FromY), (ToX, ToY)):-
+    abs(FromX - ToX) = abs(FromY - ToY) ,
+    abs(FromX - ToX) >= 0 . 
