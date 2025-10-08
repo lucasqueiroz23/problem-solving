6766c7e0ead92711d1756b1801dc4443db31dc04
Lucas Queiroz <lucas.fn4@gmail.com>
2025-10-07 23:55:09 -0300

    adds some more stuff to rs template

--- template.rs
+++ template.rs
@@ -1,9 +1,24 @@
 use std::io;
 
-fn take_int() -> usize {
+type ll = i64;
+type vi = Vec<i32>;
+type ii = (i32, i32);
+type vii = Vec<ii>;
+
+fn read_int() -> usize {
     let mut input: String = String::new();
     io::stdin().read_line(&mut input).unwrap();
     return input.trim().parse().unwrap();
+}
+
+fn read_vector() -> Vec<i32> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    return input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
 }
 
 fn main() {}
